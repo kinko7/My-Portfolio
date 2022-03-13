62a6079e87a1609cbbe28d8399a4c7133b0f1aad
kinko7 <kion7kion@gmail.com>
2022-03-13 17:41:37 -0300

    addStyles

--- src/Component/Me.js
+++ src/Component/Me.js
@@ -14,29 +14,29 @@ export default function Me() {
       <div className="let">
         <div className="card">
           <p>
-           Hi,I´m Carlos...
-            for many years I dedicated myself, as an actor and Teacher Drama, to
-            the performing arts.Times of the pandemic, created
-            the opportunity to try a positive evolution in my professional life.
-            that is how I chose to apply to Henry Bootcamp where I spent more
-            than 800 hours learning and codinng with a lot
-            of commitment, responsibility and enthusiasm to learn to progress
-            looking for a better future for me and my family. The first big
-            goal, I achieved it. Today I am a Full-Stack developer who seeks to
-            integrate an important professional team at the service of a company
-            that faces new challenges to consolidate a progressive future where,
-            from my place, will provide commitment, productive, responsible work,
-            share, and learn to grow professionally. I consider myself a
-            responsible, organized and proactive, with very good
-            management of interpersonal relationships, predisposition for
-            team work and challenges. I am interested in developing myself in
-            dynamic environments that allow me to continue learning and continue
-            developing the wonderful world of programming. Attitude, joy,
-            enthusiasm and commitment to give the best of me, are my engines in
-            this new stage of the search for a first opportunity, a job. There
-            is still a lot ahead. Effort, collaboration and constant search for
-            new solutions to improve people's lives through a responsible
-            practice tailored to that end.
+Hi,I´m Carlos...
+for many years I dedicated myself, as an actor and Teacher Drama to
+the performing arts.Times of pandemic, created
+the opportunity to try a positive evolution in my professional life.
+That is how I chose to apply to Henry Bootcamp where I spent more
+than 800 hours learning and codinng with a lot
+of commitment, responsibility and enthusiasm to learn to progress
+looking for a better future for me and my family. The first big
+goal, I achieved it. Today I am a Full-Stack developer who seeks to
+integrate an important professional team at the service of a company
+that faces new challenges to consolidate a progressive future where,
+from my place, will provide commitment, responsible work,
+and learn to grow professionally. I consider myself
+organized and proactive, with good
+management of interpersonal relationships, predisposition for
+team work and challenges. I am interested in developing myself in
+dynamic environments that allow me to continue learning and 
+developing in the wonderful world of programming. Attitude, joy,
+enthusiasm and commitment to give the best of me, are my engines in
+this new stage of the search for a first opportunity, a job. There
+is still a lot ahead. Effort, collaboration and constant search for
+new solutions to improve people's lives through a responsible
+practice tailored to that end.
           </p>
         </div>
       </div>
